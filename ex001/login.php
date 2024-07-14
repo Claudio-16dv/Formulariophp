581d19a5f4f16520dd8a2e643aa97b3d1da58134
Claudio-16dv <claudiojunior2302@uol.com.br>
2024-07-14 19:26:15 -0300

    Novo codigo

---
+++ ex001/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Usuário</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Página de Login</h1>
+    </header>
+        <section>
+            <form action="welcome.php" method="post">
+            <label for="email">E-mail:</label>
+            <input type="email" name="email" id="idemail">
+            <label for="password">Senha:</label>
+            <input type="password" name="password" id="idpassword">
+            <input type="submit" value="Entrar" >
+            </form>
+        </section>
+</body>
+</html>
