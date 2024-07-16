b66b37b8af6a29ba0b03228c80c5b738112b0dd1
Claudio-16dv <claudiojunior2302@uol.com.br>
2024-07-16 20:25:48 -0300

    Atualizado2

--- ex001/register.php
+++ ex001/register.php
@@ -1,45 +1,36 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Usuário</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header>
-        <h1>Formulário de Cadastro</h1>
-    </header>
-        <section>
-            <form action="login.php" method="post">
-            <label for="nome">Nome Completo:</label>
-            <input type="text" name="nome" id="idnome">
-            <label for="email">E-mail:</label>
-            <input type="email" name="email" id="idemail">
-            <label for="password">Crie uma senha:</label>
-            <input type="password" name="password" id="idpassword">
-            <input type="submit" value="Criar Conta" >
-            </form>
-        </section>
-
-        <?php 
-            $conexao = mysqli_connect("localhost","root","1234","cadastro");
-
-            $email = $_POST['email'];
-            $email = mysqli_real_escape_string($conexao, $email);
-            $sql = "SELECT email FROM cadastro.users WHERE email='$email'";
-            $retorno = mysqli_query($conexao, $sql);
-
-            if(mysqli_num_rows($retorno)>0){
-                echo "Usuário Cadastrado";
+    <?php 
+             $conn = new mysqli("localhost", "root", "1111", "cadastro");
+            
+            if ($conn->connect_error) {
+                die("Conexão falhou: " . $conn->connect_error);
+            }
+            
+            
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                
+                $nome = isset($_POST['nome']) ? $_POST['nome'] : '';
+                $email = isset($_POST['email']) ? $_POST['email'] : '';
+                $password = isset($_POST['password']) ? $_POST['password'] : '';
+            
+                
+                if (empty($nome) || empty($email) || empty($password)) {
+                    die("Todos os campos são obrigatórios.");
+                }
+            
+                
+                $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+            
+                
+                $sql = "INSERT INTO users (full_name, email, password) VALUES ('$nome', '$email', '$passwordHash')";
+            
+                if ($conn->query($sql) === TRUE) {
+                    echo "Registro criado com sucesso!";
+                } else {
+                    echo "Erro: " . $sql . "<br>" . $conn->error;
+                }
+            
+                $conn->close();
             } else {
-                $nome = $_POST['nome'];
-                $email = $_POST['email'];
-                $password = $_POST['password'];
-
-                $sql = "INSERT INTO cadastro.users(nome,email,password) values('$nome', '$email', '$password')";
-                $resultado = mysqli_query($conexao, $sql);
+                die("Método de solicitação inválido.");
             }
         ?>
-</body>
-</html>
