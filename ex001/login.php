f7c45d7a205da7625996cfb5cdef99758306022d
Claudio-16dv <claudiojunior2302@uol.com.br>
2024-07-19 23:45:31 -0300

    sql guardando

--- ex001/login.php
+++ ex001/login.php
@@ -11,12 +11,13 @@
         <h1>Página de Login</h1>
     </header>
         <section>
-            <form action="welcome.php" method="post">
+            <form action="test.php" method="post">
             <label for="email">E-mail:</label>
             <input type="email" name="email" id="idemail">
             <label for="password">Senha:</label>
             <input type="password" name="password" id="idpassword">
-            <input type="submit" value="Entrar" >
+            <input type="submit" name="submit" value="Entrar" >
+            <input type="button" value="Criar conta" onclick="window.location.href='register.php'">
             </form>
         </section>
 </body>
